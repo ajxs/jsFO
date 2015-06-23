a0b52d85175856420b13a8862931cb2c69fdb456
Ajxs <ajxs@users.noreply.github.com>
2015-06-23 23:10:41 +1000

    .msg file loader added
    Preliminary speech framework.

---
+++ loader/loader_msg.py
@@ -0,0 +1,39 @@
+import sys
+sys.path.insert(0, '/home/protected/py/lib/python3.4/site-packages/')
+
+def loadMSG(msgFile):
+	msgInfo = {}
+
+	msgInfo['lines'] = []
+	
+	msgLines = []
+	
+	lastIndex = 0
+	
+	#@todo - can potentially optimize this to not need first array
+	
+	lines = msgFile.readlines()
+	for i in range(len(lines)):
+		line = lines[i].decode("utf-8","ignore").strip()
+		
+		if(line):
+			if(line[0] == "#"):
+				continue
+			elif(line[0] == "{"):
+				msgLines.append(line)
+				lastIndex = len(msgLines)-1
+			else:
+				msgLines[lastIndex] += " " + line
+		
+	for i in range(len(msgLines)):
+		split = msgLines[i].split("{");
+		
+		newLine = {}
+		
+		newLine['id'] = int(split[1].replace("}",""));
+		newLine['sound'] = split[2].replace("}","");
+		newLine['text'] = split[3].replace("}","");
+		
+		msgInfo['lines'].append(newLine);
+				
+	return msgInfo
